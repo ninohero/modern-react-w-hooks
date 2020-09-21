d14bc97b0316d2a3b33cc353a76c1f0ee1d27ca0
ninohero <ninohero@muzocalo.com>
2020-09-21 15:26:16 -0400

    finished delete button and checkboxes

---
+++ src/TodoApp.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import TodoList from "./TodoList";
+import TodoForm from "./TodoForm";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Grid from "@material-ui/core/Grid";
+import { v4 as uuid } from "uuid";
+
+const TodoApp = () => {
+  const initalTodos = [
+    { id: 1, task: "Clean Fishtank", completed: false },
+    { id: 2, task: "Wash Car", completed: true },
+    { id: 3, task: "Grow Beard", completed: false },
+  ];
+  const [todos, setTodos] = useState(initalTodos);
+  const addTodo = (newTodoText) => {
+    setTodos([...todos, { id: uuid(), task: newTodoText, completed: false }]);
+  };
+  const removeTodo = (todoId) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== todoId);
+    setTodos(updatedTodos);
+  };
+  const toggleTodo = (todoId) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === todoId ? { ...todo, completed: !todo.completed } : todo
+    );
+    setTodos(updatedTodos);
+  };
+  return (
+    <Paper
+      style={{
+        padding: 0,
+        margin: 0,
+        height: "100vh",
+        backgroundColor: "#fafafa",
+      }}
+      elevation={0}
+    >
+      <AppBar color="primary" position="static" style={{ height: "64px" }}>
+        <Toolbar>
+          <Typography color="inherit">TODOS WITH HOOKS</Typography>
+        </Toolbar>
+      </AppBar>
+      <Grid container justify="center" style={{ marginTop: "1rem" }}>
+        <Grid item xs={11} md={8} lg={4}>
+          <TodoForm addTodo={addTodo} />
+          <TodoList
+            todos={todos}
+            removeTodo={removeTodo}
+            toggleTodo={toggleTodo}
+          />
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+};
+
+export default TodoApp;
---
+++ src/TodoList.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import Paper from "@material-ui/core/Paper";
+import Divider from "@material-ui/core/Divider";
+import List from "@material-ui/core/List";
+import ListItemText from "@material-ui/core/ListItemText";
+import Todo from "./Todo";
+
+const TodoList = ({ todos, removeTodo, toggleTodo }) => {
+  return (
+    <Paper>
+      <List>
+        {todos.map((todo) => (
+          <>
+            <Todo
+              id={todo.id}
+              task={todo.task}
+              key={todo.id}
+              completed={todo.completed}
+              removeTodo={removeTodo}
+              toggleTodo={toggleTodo}
+            />
+            <Divider />
+          </>
+        ))}
+      </List>
+    </Paper>
+  );
+};
+export default TodoList;
